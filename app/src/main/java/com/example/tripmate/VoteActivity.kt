000fc328fb1003e919ec6112261fba914641e46e
gilianne <giliannebaguio@gmail.com>
2025-10-18 20:34:06 +0800

    added buttom nav in vote tab

--- app/src/main/java/com/example/tripmate/VoteActivity.kt
+++ app/src/main/java/com/example/tripmate/VoteActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class VoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +22,15 @@ class VoteActivity : AppCompatActivity() {
         val tabExpenses = findViewById<Button>(R.id.tabExpenses)
         val tabDocs = findViewById<Button>(R.id.tabDocs)
         val tabItinerary = findViewById<Button>(R.id.tabItinerary)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav) // for bottomnav
+
+        // remove blue highlight in bottom nav
+        bottomNav.menu.setGroupCheckable(0, true, false)
+        for (i in 0 until bottomNav.menu.size()) {
+            bottomNav.menu.getItem(i).isChecked = false
+        }
+        bottomNav.menu.setGroupCheckable(0, true, true)
+        //----------
 
         btnSubmitVote1.setOnClickListener {
             val selected = rgPoll1.checkedRadioButtonId
@@ -60,6 +71,18 @@ class VoteActivity : AppCompatActivity() {
         }
         tabItinerary.setOnClickListener {
             startActivity(Intent(this, ItineraryActivity::class.java))
+        }
+
+        // Bottom Navigation
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> startActivity(Intent(this, DashboardActivity::class.java))
+                R.id.nav_create -> startActivity(Intent(this, MyTripsActivity::class.java))
+                R.id.nav_notifications -> startActivity(Intent(this, NotificationsActivity::class.java))
+                R.id.nav_profile -> startActivity(Intent(this, ProfileActivity::class.java))
+            }
+            overridePendingTransition(0, 0)
+            true
         }
     }
 }
